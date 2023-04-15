1868baf7ac397b42bb4a5f4e45e8af4c5fb5168a
prajapatipiyush1209 <prajapatipiyush1209@gmail.com>
2023-04-15 17:34:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## END TO END ML PROJECT
+
+### Create a environment 
+```
+conda create -p venv python=3.8
+```
+
+### Active a environment 
+```
+conda activate venv/
+```
+
+### Install all required library 
+```
+pip install -r requirements.txt
+```
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HIFEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    with open(file_path, 'r') as f :
+        read_requirements = f.readlines()
+        read_requirements = [i.replace("\n", "") for i in read_requirements]
+        if HIFEN_E_DOT in read_requirements:
+            read_requirements.remove(HIFEN_E_DOT)
+
+    return read_requirements
+
+
+
+setup (name= 'RegressionProject',
+       version = '0.0.1',
+       author="Piyush Prajapati",
+       author_email="prajapatipiyush1209@gmail.com",
+       packages=find_packages(),
+       install_requires = get_requirements('requirements.txt')
+)
+
+
+
